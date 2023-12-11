7d3c2944f6e5fc09bade057679217e4d6702519e
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-11 02:32:50 -0300

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@
 const authMiddleware = (req, res, next) => {
   if (!req.session || !req.session.user) {
     console.log('Usuario no autenticado. Redirigiendo...');
-    return res.redirect('/auth/login');
+    return res.status(401).json({ message: 'Usuario no autenticado' });
   }
   console.log('Usuario autenticado:', req.session.user);
   next();
